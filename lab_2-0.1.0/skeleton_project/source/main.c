ac851d906bc04d45cb67f4e8934ae0443b946ea6
tuuvaab <tuvbj@stud.ntnu.no>
2024-02-26 17:22:56 +0100

    Copied obstruction and stopbutton-function back into main

--- lab_2-0.1.0/skeleton_project/source/main.c
+++ lab_2-0.1.0/skeleton_project/source/main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <time.h>
-//#include <pthread.h>
 #include "driver/elevio.h"
 #include "driver/elevator.h"
 #include "driver/globalVariables.h"
@@ -30,7 +29,7 @@ int main(){
         //At destination
         if(*ptrToHead != NULL && (*ptrToHead)->floorLevel == elev.currentFloor){
             if(previousFloor != elev.currentFloor){
-                time(&startCountDoor);
+                time(&startCountDoor); //Restart timer
                 elevio_motorDirection(DIRN_STOP);
                 removeFromQue(elev.currentFloor);
                 for(int b = 0; b < N_BUTTONS; b++){
@@ -43,6 +42,21 @@ int main(){
         doorBlocksDrive = currentTime  >= startCountDoor + 3 ? 0 : 1; 
         if(!doorBlocksDrive){
             driveElevator();
+        }
+
+        //Checking stop button
+        if(elevio_stopButton()){
+            elevio_motorDirection(DIRN_STOP);
+            //Holding until deactivated
+            time(&startCountDoor);
+            break;
+        }
+
+        //Obstruction light
+        if(elevio_obstruction()){
+            elevio_stopLamp(1);
+        } else {
+            elevio_stopLamp(0);
         }
         
         //Checking all buttons, 
@@ -61,35 +75,6 @@ int main(){
     }
     /*
     while(1){
-        int floor = elevio_floorSensor();
-
-        if(floor == 0){
-            elevio_motorDirection(DIRN_UP);
-        }
-
-        if(floor == N_FLOORS-1){
-            elevio_motorDirection(DIRN_DOWN);
-        }
-
-
-        for(int f = 0; f < N_FLOORS; f++){
-            for(int b = 0; b < N_BUTTONS; b++){
-                int btnPressed = elevio_callButton(f, b);
-                elevio_buttonLamp(f, b, btnPressed); //TODO, implement hold on light after button released
-            }
-        }
-
-        if(elevio_obstruction()){
-            elevio_stopLamp(1);
-        } else {
-            elevio_stopLamp(0);
-        }
-        
-        if(elevio_stopButton()){
-            elevio_motorDirection(DIRN_STOP);
-            break;
-        }
-        
         nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
     }
     */
